e16e910b64bed4bfe22b40f13ffa7f23e9bebde3
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-05-20 17:22:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #set -x
 
+RED='\033[1;31m' # alarm
+GRN='\033[1;32m' # notice
+YEL='\033[1;33m' # warning
+NC='\033[0m' # No Color
+
 source env.txt
 
 LOGINRESPONSE=`curl -s "https://${URL}/v3-public/localProviders/local?action=login" -H 'content-type: application/json' --data-binary '{"username":"'${USER}'","password":"'${PASSWORD}'"}' --insecure`
@@ -11,11 +16,11 @@ APIRESPONSE=`curl -s "https://${URL}/v3/token" -H 'content-type: application/jso
 
 APITOKEN=`echo $APIRESPONSE | jq -r .token`
 
-echo "Please copy the following token:"
+printf "${GRN}==Please copy the following token:==${NC}\n"
 echo -e "${APITOKEN}\n"
 
 ## Create Custom Cluster
-helm install --namespace fleet-default --values charts/values.yaml do-cluster ./charts
+helm install --namespace fleet-default --values charts/values.yaml do-cluster ./charts &>/dev/null
 
 ## Show Registration Command
 CLUSTERNAME=$(cat charts/values.yaml | sed 's/^[[:space:]]*//' | grep "^name:" | cut -d ' ' -f2 | tr -d '\r')
@@ -24,6 +29,6 @@ CLUSTERID=$(kubectl get clusters.management.cattle.io -o yaml | grep -A20 "$CLUS
 
 VAR="\"clusterId\": \"${CLUSTERID}\""
 
-echo "Please copy the Registration Command:"
+printf "${GRN}==Please copy the Registration Allinone Command:==${NC}\n"
 CODE=$(curl -s -k -H "Authorization: Bearer ${APITOKEN}" https://"$URL"/v3/clusterregistrationtokens | jq | grep -A7 "$VAR" | grep insecureNodeCommand | cut -d '"' -f4 | sed 's/^[[:space:]]*//')
 echo "$CODE --etcd --controlplane --worker"
